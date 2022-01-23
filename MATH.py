75b6d42274f828b7023dadb6fdf69fac96d0fb19
ANJAN  KUMAR DEY <63156047+anjandey-tech@users.noreply.github.com>
2022-01-23 19:13:34 +0530

    initial commit of math.py

---
+++ MATH.py
@@ -0,0 +1,20 @@
+#Add Implementation
+def add(x,y):
+      return x+y
+#Add Implementation
+def subtract(x,y):
+      return x-y      #this is on master branch
+
+#Add Implementation
+def multiply(x,y):
+      return x*y   #this is on bug456
+
+#Add Implementation
+def devide(x,y):
+      if  y==0;
+      return DEVIDE_BY_0_ERROR       #this is on bug789
+      else :
+      return x/y
+#Square Implementation
+ def square(X):
+ return x*x
